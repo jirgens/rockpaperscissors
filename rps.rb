6fda462643a4a1d5ded23fcbc633319dee855a7b
jirgens <jessa.irg@gmail.com>
2015-09-26 12:04:36 -0500

    .

--- rps.rb
+++ rps.rb
@@ -1,6 +1,36 @@
+CHOICES = {'p' => 'Paper', 'r' => "Rock", 's' => "Scissors"}
+
+def display_winning_message(winning_choice)
+  case winning_choice
+  when 'p'
+    puts 'Paper wraps rock!'
+  when 'r'
+    puts 'Rock crushes scissors!'
+  when 's'
+    puts 'Scissors cut Paper!'
+  end
+end
+
 puts "Let's play Rock Paper Scissors!"
-puts "Choose one: (R/P/S)"
 
-play = gets.chomp
 
-puts "You picked #{play}. The computer picked..."
+loop do
+  #player chooses
+  begin
+    puts "Choose one: (R/P/S)"
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
+  #computer chooses
+  computer_choice = CHOICES.keys.sample
+
+  if player_choice == computer_choice
+    puts "It's a tie"
+  elsif (player_choice == 'p' && computer_choice == 'r') || (player_choice == 'r' && computer_choice == 's') || (player_choice == 's' && computer_choice == 'p')
+    display_winning_message(player_choice)
+    puts "You win"
+  else
+    display_winning_message(computer_choice)
+    puts "Computer won!"
+  end
+
+end
